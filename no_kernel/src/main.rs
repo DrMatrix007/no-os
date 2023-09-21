4d91d1b5077c138c22029d50698286c00c2d3e0b
Ofri Haviv <haviv.ofri@gmail.com>
2023-09-21 14:02:26 +0300

    added uefi support

--- no_kernel/src/main.rs
+++ no_kernel/src/main.rs
@@ -5,14 +5,12 @@ use core::panic::PanicInfo;
 
 use bootloader_api::BootInfo;
 
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 fn no_kernel_main(boot_info: &'static mut BootInfo) -> ! {
     // let vga_buffer = 0xb8000 as *mut u8;
     boot_info.framebuffer.as_mut().unwrap().buffer_mut().iter_mut().map(|a|*a = 0xFF).count();
-
-    loop{}
+    panic!()
 
 }
 bootloader_api::entry_point!(no_kernel_main);
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
-use std::{path::Path, process::Command};
-
-use bootloader::BootConfig;
+use std::{env, path::Path, process::Command};
 
 fn main() {
-    let boot= bootloader::BiosBoot::new(Path::new("./kernel_build/kernel.no"));
+    let mut args = env::args();
+    args.next();
+    let t = args.next();
 
-    boot.create_disk_image(Path::new("./kernel_build/kernel.img")).unwrap();
+    match t.as_deref() {
+        Some("uefi") => {
+            println!("running UEFI!");
+            let boot = bootloader::UefiBoot::new(Path::new("./kernel_build/kernel.no"));
 
- let status = Command::new("qemu-system-x86_64")
-        .arg("-drive")
-        .arg("format=raw,file=./kernel_build/kernel.img")
-        .status()
-        .expect("Failed to start qemu-system-x86_64");
+            boot.create_disk_image(Path::new("./kernel_build/kernel_uefi.img"))
+                .unwrap();
 
+            Command::new("qemu-system-x86_64")
+                .arg("-drive")
+                .arg("format=raw,file=./kernel_build/kernel_uefi.img")
+                .args(["-drive", "if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd"])
+                .args(["-drive", "if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd"])
+                .status()
+                .expect("Failed to start qemu-system-x86_64");
+        }
+        _ => {
+            println!("running BIOS!");
+            let boot = bootloader::BiosBoot::new(Path::new("./kernel_build/kernel.no"));
 
+            boot.create_disk_image(Path::new("./kernel_build/kernel_bios.img"))
+                .unwrap();
+
+            Command::new("qemu-system-x86_64")
+                .arg("-drive")
+                .arg("format=raw,file=./kernel_build/kernel_bios.img")
+                .status()
+                .expect("Failed to start qemu-system-x86_64");
+        }
     }
+}
